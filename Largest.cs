1bf67a1b9f75b54d2bc66be33fc1a7171c4ba92e
Amanuel Legesse <85198830+Skwalker416@users.noreply.github.com>
2022-02-06 13:23:06 +0300

    Update Largest.cs

--- Largest.cs
+++ Largest.cs
@@ -10,12 +10,12 @@ namespace  C__Assignment_1
 	
 	           int[] myArray = new int[] { 67, 8, 24, 50, 13, 10, 5 };
 
-                    int largest = int.MaxValue;
+                    int largest = int.MinValue;
                     int second = int.MinValue;
             foreach (int i in myArray)
         {
             if (i > largest)
-        {
+       
                 second = largest;
                 largest = i;
         }
